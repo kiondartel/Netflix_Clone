c93643cd845277ac8b1a116023f3a4bf1a11d34f
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-10-27 18:07:40 -0300

    styles Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -14,7 +14,7 @@ export const Banner = () => {
       );
       const data = await getMovies(netFlixOriginalCategory.path);
       const movies = data?.results;
-      const randomIndex = Math.floor(Math.random() * data.lenght);
+      const randomIndex = Math.floor(Math.random() * movies.length);
       setMovie(movies[randomIndex])
     } catch (err) {
       console.log("Baner fetchRandomMovie:", err);
@@ -27,9 +27,27 @@ export const Banner = () => {
 
 
   return (
-    <Styled.Container className="banner-content">
+    <Styled.Header className="banner-container" style={{
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      roundPosition: "center"
+    }}>
 
-    </Styled.Container>
+      <Styled.Container className="banner-content">
+        <Styled.H1>{movie?.title || movie?.name || movie?.original_name}</Styled.H1>
+      </Styled.Container>
+
+      <Styled.Container className="button-content">
+        <Styled.Button>Assistir</Styled.Button>
+        <Styled.Button>Minha Lista</Styled.Button>
+      </Styled.Container>
+
+      <Styled.Container>
+        <Styled.P>
+          {movie?.overview}
+        </Styled.P>
+      </Styled.Container>
+
+    </ Styled.Header>
   );
 };
 
--- src/components/Banner/styles.js
+++ src/components/Banner/styles.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components';
 
+export const Header = styled.header`
+  color: white;
+  object-fit: contain;
+  height: 448px;
+  `;
+
 export const Container = styled.div`
+`;
+
+export const H1 = styled.h1`
+  font-size: 3rem;
+  font-weight: 800px;
+  padding-top: 8rem;
+  padding-left: 2rem;
+`;
+
+export const P = styled.p`
+  width: 45rem;
+  line-height: 1.3rem;
+  padding-top: 1rem;
+  font-size: 1rem;
+  max-width: 360px;
+  height: 80px;
+  margin-left: 2rem;
+`;
+
+
+export const Button = styled.button`
+  cursor: pointer;
+  color: #fff;
+  outline: none;
+  border: none;
+  font-weight: 700;
+  border-radius: 0.2vw;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  margin-left: 3.5rem;
+  background-color: rgba(51, 51, 51, 0.5);
   
+  &:hover{
+    color: #000;
+    background-color: #e6e6e6;
+    transition: 0.2s;
+  }
 `;
